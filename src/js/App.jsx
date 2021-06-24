2df8b0a67fcd74f4f7b507c2c910a8415cc9201d
arjanDhakal <dhakal.arjan@gmail.com>
2021-06-24 14:02:02 +0545

    Boiler Plate for chrome extension

---
+++ src/js/App.jsx
@@ -0,0 +1,9 @@
+import { h, render, Component } from "preact";
+
+const App = () => (
+  <div class="flex items-center">
+    <h1>Boiler Plate</h1>
+  </div>
+);
+
+export default App;
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import { h, render, Component } from "preact";
+import App from "./App";
+
+document.addEventListener("DOMContentLoaded", () => {
+  render(<App />, document.body);
+});
